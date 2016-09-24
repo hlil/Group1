f3c11dece160f75f020fee9e25c8f503ac985d81
HahuIt <hahutraining@gmail.com>
2016-09-24 13:53:50 -0700

    Add files via upload

---
+++ AoutoInsuranceDB/PolicyTable.sql
@@ -0,0 +1,14 @@
+use AoutoinsuranceDb1
+GO
+CREATE TABLE [policy](
+	[PolicyID] [int] NOT NULL,
+	[PolicyEffectiveDate] [datetime] NULL,
+	[PolicyExpireDate] [datetime] NULL,
+	[PaymentOption] [varchar](100) NULL,
+	[TotalAmount] [money] NULL,
+	[Active] [real] NULL,
+	[AdditionalInfo] [text] NULL,
+	[CreatedDate] [datetime] NULL
+)
+Drop table polisies
+GO
